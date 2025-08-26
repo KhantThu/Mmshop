b4295ed557919184bebf8b54f2e78de9bf8c9e59
KhantThu <kk4793510@gmail.com>
2025-08-27 04:40:03 +0700

    testfail

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -20,6 +20,10 @@ describe("AppController (e2e)", () => {
     return request(app.getHttpServer())
       .get("/")
       .expect(200)
-      .expect("Hello World!");
+      .expect({
+        up: true,
+        ready: true,
+        message: 'App is running',
+      });
   });
 });
